d91be8f9dd57c012a261bc827117660e4113d49f
Dmytro Kotenko <2231913@my.ipleiria.pt>
2025-09-17 22:28:17 +0100

    adicionados todos os componentes visuais, falta logica

--- app/src/main/java/ipleiria/eec/pdm/MainActivity.java
+++ app/src/main/java/ipleiria/eec/pdm/MainActivity.java
@@ -1,14 +1,21 @@
 package ipleiria.eec.pdm;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.graphics.ColorUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNumero1;
+    private EditText txtNumero2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtNumero1 = findViewById(R.id.textViewNum1);
+        txtNumero2 = findViewById(R.id.textViewNum2);
+    }
+
+    public void onClickSoma(View view) {
+
+        if (txtNumero1.getText().toString().trim().isEmpty() || txtNumero2.getText().toString().trim().isEmpty()) {
+            return;
+        }
+
+        /*
+        if (Numero1.getText().toString().trim().equals("") || Numero2.getText().toString().trim().equals("")) {
+            return;
+        }
+        int numero1 = Integer.parseInt(Numero1.getText().toString());
+        int numero2 = Integer.parseInt(Numero2.getText().toString());
+        int soma = numero1 + numero2;
+        String resultado = getResources().getString(R.string.txtResultado) + " " + soma;
+        Numero1.setText("");
+        Numero2.setText("");
+        Numero1.requestFocus();
+        Toast.makeText(this, resultado, Toast.LENGTH_LONG).show();
+        */
+
     }
 }
